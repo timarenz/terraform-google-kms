cf918618a98c6cb684ad55525cff370b0482b6c9
Tim Arenz <tim@arenz.cc>
2019-09-18 10:28:28 +0200

    removing service account and randomness

--- main.tf
+++ main.tf
@@ -4,27 +4,10 @@ resource "google_project_service" "main" {
   disable_on_destroy = var.disable_kms_api_on_destroy
 }
 
-resource "google_service_account" "main" {
-  account_id   = "${var.name}"
-  display_name = "${var.name}"
-  project      = var.project
-}
-
-resource "google_service_account_key" "main" {
-  service_account_id = google_service_account.main.name
-  private_key_type   = "TYPE_GOOGLE_CREDENTIALS_FILE"
-}
-
-resource "random_string" "kms_key_ring" {
-  length  = 4
-  special = false
-  upper   = false
-}
-
 resource "google_kms_key_ring" "main" {
   depends_on = [google_project_service.main]
   project    = var.project
-  name       = "${var.name}-${random_string.kms_key_ring.result}"
+  name       = var.name
   location   = var.region
 }
 
@@ -32,13 +15,4 @@ resource "google_kms_crypto_key" "main" {
   name            = "${var.name}"
   key_ring        = google_kms_key_ring.main.self_link
   rotation_period = "100000s"
-}
-
-resource "google_kms_key_ring_iam_binding" "main" {
-  key_ring_id = google_kms_key_ring.main.self_link
-  role        = "roles/owner"
-
-  members = [
-    "serviceAccount:${google_service_account.main.email}",
-  ]
 }
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-output "service_accout_id" {
-  value = google_service_account.main.unique_id
-}
-
-output "service_accout_name" {
-  value = google_service_account.main.name
-}
-
-output "service_accout_email" {
-  value = google_service_account.main.email
-}
-
 output "key_ring_self_link" {
   value = google_kms_key_ring.main.self_link
 }
@@ -28,10 +16,6 @@ output "crypto_key_id" {
 
 output "crypto_key" {
   value = google_kms_crypto_key.main.name
-}
-
-output "google_credentials_file" {
-  value = base64decode(google_service_account_key.main.private_key)
 }
 
 output "region" {
